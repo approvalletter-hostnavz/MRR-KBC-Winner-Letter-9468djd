1246df258714eb3fc8aba42f74961f0f289a2d62
approvalletter-hostnavz <161006194+approvalletter-hostnavz@users.noreply.github.com>
2025-03-19 10:28:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ function getCurrentDate() {
 
 // Function to check if the page is still valid
 async function checkPageValidity() {
-  const expiryDate = new Date('2025-03-09T09:49:00'); // yyyy-mm-ddThh:mm:ss Format
+  const expiryDate = new Date('2025-04-16T10:26:00'); // yyyy-mm-ddThh:mm:ss Format
   const currentDate = await getCurrentDate();
 
   if (currentDate > expiryDate) {
